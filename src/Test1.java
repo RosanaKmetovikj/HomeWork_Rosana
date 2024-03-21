95fba02ab38aba6988da376a4aec2cba051364ce
Rosana <rosanakocova@yahoo.com>
2024-03-21 22:44:44 +0100

    New classes are created!

--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
         System.out.println("Test 1");
         System.out.println("Test 2 needs to be validated");
         System.out.println("Test 3 is here to support test 2");
+        //here i need to practice more GIT
     }
 }
