28f015b2b1274e16ba4570fda19a6a2ae191d68f
Rosana <rosanakocova@yahoo.com>
2024-03-21 22:40:28 +0100

    New classes are created!

--- src/Test4.java
+++ src/Test4.java
@@ -1,5 +1,6 @@
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("See what it`s inside test 4");
+        System.out.println("I think you already know that");
     }
 }
