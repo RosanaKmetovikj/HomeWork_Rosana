5c4009672a58b35c50930bef43a892ea49ef62a8
Rosana <rosanakocova@yahoo.com>
2024-03-21 22:39:50 +0100

    New classes are created!

---
+++ src/Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("New tests are created");
+        System.out.println("New tests are created and needs to be executed");
+    }
+}
---
+++ src/Test4.java
@@ -0,0 +1,5 @@
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("See what it`s inside test 4");
+    }
+}
