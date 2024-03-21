6d77a4ef05df9d6d3be8fbb36c15fb1cf4e3d1ed
Rosana <rosanakocova@yahoo.com>
2024-03-21 22:48:53 +0100

    New classes are created!

--- src/Test2.java
+++ src/Test2.java
@@ -2,5 +2,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("New tests are created");
         System.out.println("New tests are created and needs to be executed");
+        System.out.println("this is an update");
     }
 }
--- src/Test3.java
+++ src/Test3.java
@@ -2,5 +2,6 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("I hate loops");
         System.out.println("I know you know that I hate loops but I will say it again");
+        System.out.println("I still hate loops but i have to practice them");
     }
 }
