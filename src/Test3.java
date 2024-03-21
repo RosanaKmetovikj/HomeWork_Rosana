c45a5dba9d2875f64085b8130f7292166e55cde8
Rosana <rosanakocova@yahoo.com>
2024-03-21 22:43:31 +0100

    New classes are created!

--- src/Test3.java
+++ src/Test3.java
@@ -1,5 +1,6 @@
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("I hate loops");
+        System.out.println("I know you know that I hate loops but I will say it again");
     }
 }
